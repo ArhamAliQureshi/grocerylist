4d015f6f74aeca3c3921b18d95995550577407cd
arhamaliqureshi <arhamaliqureshi@yahoo.com>
2013-10-29 16:45:20 +0500

    Following changes: 1) Cart system enhanced. Now only stauts = false  items or items bought previously from the same care will apear.

--- js/module.js
+++ js/module.js
@@ -51,9 +51,16 @@ appModule
     })
     .directive('ngClass',function(){
         return{
-//            require: 'ngModel',
             link: function(scope, element, attrs, ctrl) {
                 if(scope.product.status==true){scope.itemBought = true}
+            }
+        }
+    })
+    .directive('ngDebug',function(){
+        return{
+            restrict: 'A',
+            link: function (scope, element, attrs, ctrl){
+                console.log(scope.$eval(attrs.ngDebug));
             }
         }
     });
@@ -69,6 +76,17 @@ appModule
             }
             return input;
         };
+    })
+    .filter('status', function(){        // This will filter cart item and return items which status == true/false OR cartID == cartNumber.
+        return function(input, attr1, attr2){   //Obj, bool, cartNumber
+            var filtered = [];  //contains the filtered product.
+            angular.forEach(input,function(value,key){
+                if(value.status == attr1 || (value.cartID == attr2)){
+                    filtered.push(value);
+                }
+            });
+            return filtered;
+        }
     });
 
 
@@ -79,8 +97,8 @@ appModule
             {item:'Potato', quantity: 3, status: false, unitPrice: 0, cost: 0, cartID: '0'},
             {item:'Tomato', quantity: 1, status: false, unitPrice: 0, cost: 0, cartID: '0'},
             {item:'Banana', quantity: 2, status: false, unitPrice: 0, cost: 0, cartID: '0'}
-
         ];
+        window.test = products;
         var activeRow = products.length;       //Contains the index of the click row in edit mode.
         return{
             products: function(){
@@ -133,8 +151,6 @@ appModule
 
         };
 
-        $scope.cartNumber = 1;
-
         $scope.removeRow = function(){
             $scope.activeRow = dataService.activeRow();
             dataService.products().splice($scope.activeRow,1);
@@ -163,7 +179,7 @@ appModule
 
     })
     .controller('cartCtrl',function($scope, $routeParams,dataService){
-        $scope.cartNumber = $routeParams;
+        $scope.cartNumber = $routeParams.cartNumber;
         $scope.products = dataService.products();       //Get products Array from service.
 
         $scope.clickRow = function(element){
